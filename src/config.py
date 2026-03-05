f1c10438c230f00e458de6c930084a45176e8b1b
santvz6 <sant.alvz84@gmail.com>
2026-03-05 12:38:32 +0100

    base

--- src/config.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-class Config:
-    def __init__(self):
-        pass
---
+++ src/config/paths.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent # ../src/config/paths.py (3 levels)
+
+
+class Paths:
+    ROOT = BASE_DIR
+    SRC = ROOT / "src"
+    INTERNAL = ROOT / "internal"
+   
+    DATA_DIR = INTERNAL / "data"   
+    EXPERIMENTS_DIR = INTERNAL / "experiments"
+    LOGS_DIR = INTERNAL / "logs"
+    TEMP_DIR = INTERNAL / "temp"
+
+    SMPL_DIR = DATA_DIR / "smpl"
+
+
+    @classmethod
+    def init_project(cls):
+        dirs = [
+            cls.INTERNAL,
+            cls.DATA_DIR,  cls.EXPERIMENTS_DIR, cls.LOGS_DIR, cls.TEMP_DIR
+        ]
+        for d in dirs:
+            d.mkdir(parents=True, exist_ok=True)
+        
+        cls._rotate_logs()
+
+    @classmethod
+    def _rotate_logs(cls, max_logs=10):
+        existing_logs = sorted(
+            [f for f in cls.LOGS_DIR.glob("*.log")], 
+            key=os.path.getmtime
+        )
+        if len(existing_logs) >= max_logs:
+            for log_file in existing_logs[:(len(existing_logs) - max_logs + 1)]:
+                shutil.move(str(log_file), str(cls.TEMP_DIR / log_file.name))
+
